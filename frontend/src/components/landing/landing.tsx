2586cb2353ab380ff5194dbe45a7c5728c6b054d
Tsungai Chipato <tsungai@kryha.io>
2022-05-06 14:00:41 +0200

    remove landing

--- frontend/src/components/landing/landing.tsx
+++ frontend/src/components/landing/landing.tsx
@@ -1,46 +1,7 @@
 import { FC } from "react";
-import { FilteringMask } from "../../assets";
-import { MenuItem } from "../../view";
 
 export const Landing: FC = () => {
   return (
-    <><h1>Landing</h1><MenuItem items={[
-      {
-        name: "fasce jamsdsd",
-        id: "123313",
-        image: FilteringMask,
-        equipped: true,
-        category: "mask",
-        price: 12.673278,
-        amount: 23,
-      },
-      {
-        name: "fasce jamsdsd",
-        id: "123313",
-        image: FilteringMask,
-        equipped: false,
-        category: "mask",
-        price: 12.673278,
-        amount: 23,
-      },
-      {
-        name: "fasce jamsdsd",
-        id: "123313",
-        image: FilteringMask,
-        equipped: false,
-        category: "mask",
-        price: 12.673278,
-        amount: 23,
-      },
-      {
-        name: "fasce jamsdsd",
-        id: "123313",
-        image: FilteringMask,
-        equipped: false,
-        category: "mask",
-        price: 12.673278,
-        amount: 23,
-      },
-    ]} /></>
+    <h1>Landing</h1>
   );
 };
