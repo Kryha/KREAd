56277107f3904cd44b312bd2642dd671fcfbd3b3
Tsungai Chipato <tsungai@kryha.io>
2022-05-11 14:59:23 +0200

    wip

--- frontend/src/view/atoms/button.ts
+++ frontend/src/view/atoms/button.ts
@@ -10,8 +10,8 @@ interface ButtonProps {
 }
 
 export const ButtonBase = styled.button<ButtonProps>`
-  &::first-letter {
-    text-transform: uppercase;
+  ::first-letter {
+    text-transform: capitalize;
   };
   font-family: Aktiv Grotesk Medium;
   display: inline-block;
@@ -24,7 +24,7 @@ export const ButtonBase = styled.button<ButtonProps>`
   flex-direction: row;
   align-items: center;
   padding: 3px 8px 3px 16px;
-  background: ${(props): string => props.backgroundColor || color.black};
+  background: ${(props): string => props.backgroundColor || color.limeGreen};
   font-weight: ${fontWeight.light};
   color: ${(props): string => props.fontColor || color.black};
   border: none;
@@ -45,166 +45,7 @@ export const ButtonBase = styled.button<ButtonProps>`
   }};
 `;
 
-export const PrimaryButton = styled.button<ButtonProps>`
-  &::first-letter {
-    text-transform: uppercase;
-  };
-  transition: all 0.4s ease 0s;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding: 13px 20px;
-  cursor: pointer;
-  border-radius: 24px;
-  font-family: Aktiv Grotesk Medium;
-  font-weight: ${fontWeight.light};
-  font-size: 14px;
-  line-height: 18px;
-  border: 2px solid ${color.black};
-  box-sizing: border-box;
-  display: flex;
-  align-items: center;
-  letter-spacing: 0.05em;
-  text-transform: uppercase;
-  background: ${(props): string => props.backgroundColor || color.black};
-  color: ${(props): string => props.fontColor || color.white};
-  &:hover {
-    background: ${(props): string => props.backgroundColor || color.white};
-    border: 2px solid ${color.black};
-    color: ${color.black};
-    box-sizing: border-box;
-  }
-  &:active {
-    background: ${(props): string => props.backgroundColor || color.black};
-    border: 2px solid ${color.darkGrey};
-    box-sizing: border-box;
-  }
-  &:focus {
-    border: 2px solid ${color.darkGrey};
-    outline: none;
-    box-sizing: border-box;
-  }
-  ${({ disabled }): string => {
-    return disabled
-      ? `
-      color: ${color.white};
-      background: ${color.grey};
-      opacity: 0.4;
-      border: 2px solid ${color.grey};
-      &:hover {
-        color: ${color.white};
-        background: ${color.grey};
-        opacity: 0.4;
-        border: 2px solid ${color.grey};
-      }
-      &:focus {
-        border: 2px solid ${color.grey};
-      }
-      &:active {
-        border: 2px solid ${color.grey};
-      }
-      && {
-        cursor: default;
-      }
-        `
-      : "";
-  }};
-`;
-
-export const SecondaryButton = styled(PrimaryButton) <ButtonProps>`
-  border: 1px solid ${(props): string => props.borderColor || color.grey};
-  background-color: ${(props): string => props.backgroundColor || color.white};
-  color: ${(props): string => props.fontColor || color.black};
-  padding: 8px 8px 8px 16px;
-  > svg {
-    margin: 0px 6px;
-  }
-  &:hover {
-    border: 1px solid ${color.black};
-    background-color: ${color.white};
-    color: ${color.black};
-  }
-  &:active {
-    background: ${color.lightGrey};
-    border: 1px solid ${color.black};
-    color: ${color.black};
-    box-sizing: border-box;
-  }
-  &:focus {
-    border: 2px solid ${color.darkGrey};
-    outline: none;
-    box-sizing: border-box;
-  }
-  ${({ disabled }): string => {
-    return disabled
-      ? `
-      color: ${color.grey};
-      background: ${color.white};
-      border: 1px solid ${color.grey};
-      &:hover {
-        color: ${color.grey};
-        background: ${color.white};
-        border: 1px solid ${color.grey};
-      }
-      &:focus {
-         color: ${color.grey};
-        background: ${color.white};
-        border: 1px solid ${color.grey};
-      }
-      &:active {
-         color: ${color.grey};
-        background: ${color.white};
-        border: 1px solid ${color.grey};
-      }
-      && {
-        cursor: default;
-      }
-        `
-      : "";
-  }};
-`;
-
-export const TertiaryButton = styled(PrimaryButton) <ButtonProps>`
-  border: none;
-  outline: none;
-  &:hover {
-    background: ${(props): string => props.backgroundColor || "transparent"};
-    border: none;
-    outline: none;
-    box-sizing: border-box;
-  }
-  &:active {
-    background: ${(props): string => props.backgroundColor || "transparent"};
-    border: none;
-    outline: none;
-    box-sizing: border-box;
-  }
-  &:focus {
-    border: 2px solid ${color.black};
-    box-sizing: border-box;
-  }
-  ${({ disabled }): string => {
-    return disabled
-      ? `
-      color: ${color.white};
-      &:hover {
-        color: ${color.white};
-        opacity: 0.4;
-      }
-      &:focus {
-        border: 2px solid${color.black};
-      }
-      &:active {
-      }
-      && {
-        cursor: default;
-      }
-        `
-      : "";
-  }};
-`;
-
-export const OutlinedButton = styled(ButtonBase) <ButtonProps>`
+export const OutlinedButton = styled(ButtonBase)`
   border: ${(props): string => `1px solid ${props.borderColor || color.grey}}`};
   background: ${(props): string => props.backgroundColor || "transparent"};
   color: ${(props): string => props.fontColor || color.black};
