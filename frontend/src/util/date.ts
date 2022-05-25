9e2acf6dfb5a5a2eef984735e4bd88de17e58d1b
Ramiro Ramirez <ramiro@kryha.io>
2022-05-25 17:43:07 +0200

    Fix date parser

--- frontend/src/util/date.ts
+++ frontend/src/util/date.ts
@@ -1,6 +1,6 @@
 export const getDatefromEpoch = (epoch: number) => {
   const date = new Date(epoch);
-  const day = date.getDay().toString().length === 1 ? `0${date.getDay()}` : date.getDay();
+  const day = date.getDay();
   const parsedDay = day.toString().length === 1 ? `0${day}` : day;
   const month = date.getMonth() + 1;
   const parsedMonth = month.toString().length === 1 ? `0${month}` : month;
