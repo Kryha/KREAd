fe34bb656d7d359ca9d2c2643664b52c6d439b9a
Tsungai Chipato <tsungai@kryha.io>
2022-05-30 11:42:28 +0200

    add base card

--- frontend/src/pages/create-character/create-character.tsx
+++ frontend/src/pages/create-character/create-character.tsx
@@ -1,11 +1,36 @@
 import { FC } from "react";
+import { useNavigate } from "react-router-dom";
+import { DefaultIcon, text } from "../../assets";
+import { MenuText } from "../../components";
 
 import { PageContainer } from "../../components/page-container";
+import { useViewport } from "../../hooks";
+import { routes } from "../../navigation";
+import { ArrowContainer, Close, DefaultImage, Divider, FormCard, HeaderContainer } from "./styles";
 
 export const CreateCharacter: FC = () => {
+  const { width, height } = useViewport();
+  const navigate = useNavigate();
   return (
-    <>
-      <PageContainer mainContent={<></>} sidebarContent={<h2>Sidebar Content</h2>} />
-    </>
+    <PageContainer
+      mainContent={
+        <DefaultImage
+          src={DefaultIcon}
+          alt={text.character.defaultCharacter}
+          hei={height}
+          wid={width}
+        />}
+      sidebarContent={
+        <FormCard>
+          <HeaderContainer>
+            <MenuText>{text.mint.mintNewCharacter}</MenuText>
+            <Divider />
+            <ArrowContainer>
+              <Close onClick={() => navigate(routes.root)} />
+            </ArrowContainer>
+          </HeaderContainer>
+        </FormCard>
+      }
+    />
   );
 };
--- frontend/src/pages/create-character/styles.ts
+++ frontend/src/pages/create-character/styles.ts
@@ -1,1 +1,58 @@
 import styled from "styled-components";
+import { CloseIcon } from "../../assets";
+import { HeaderHorizontalDivider, Img, MenuText } from "../../components";
+import { color, margins } from "../../design";
+
+interface ImageProps {
+  hei: number;
+  wid: number;
+}
+
+export const DefaultImage = styled(Img) <ImageProps>`
+  margin-left: 140px;
+  top: 0;
+  ${({ wid, hei }): string => `min-width: ${wid * 0.4}px; max-width: ${wid * 0.4}px; width: ${wid * 0.4}px; height: ${hei}px;`};
+`;
+
+export const FormCard = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 40px;
+  width: 526px;
+  height: 688px;
+  background: ${color.gradientLight};
+  border: 1px solid ${color.grey};
+  border-radius: 24px;
+  margin-top: 40px;
+`;
+
+export const Divider = styled(HeaderHorizontalDivider)`
+  transform: rotate(90deg);
+  width: ${margins.big};
+`;
+
+export const ArrowContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  box-sizing: border-box;
+  border-radius: ${margins.medium};
+`;
+
+export const Close = styled(CloseIcon)`
+  width: ${margins.big};
+  height: ${margins.big};
+  cursor: pointer;
+`;
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  padding: 0px;
+  ${MenuText} {
+    margin-right: 60px;
+  }
+`;
