1e999de0fe12fc35c74a5e3725620b6f57e5497f
Ramiro Ramirez <ramiro@kryha.io>
2022-06-02 12:34:40 +0200

    Remove logs

--- frontend/src/util/selected.ts
+++ frontend/src/util/selected.ts
@@ -2,6 +2,5 @@ import { Item } from "../interfaces";
 
 export const isSelected = (elementList: Item[], elementId: string): boolean => {
   const elementFound = elementList.filter((element) => element.id === elementId);
-  console.log(!!elementFound.length);
   return !!elementFound.length;
 };
