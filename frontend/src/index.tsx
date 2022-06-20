584c0bc0c747d94eba3e78a57373ea04af48e295
Carlos Trigo <carlos@kryha.io>
2022-06-20 12:36:23 +0200

    remove unused cra boilerplate

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,9 +6,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-// import reportWebVitals from "./reportWebVitals";
-
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
@@ -16,13 +13,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
