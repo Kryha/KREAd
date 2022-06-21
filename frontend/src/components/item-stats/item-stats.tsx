3e01a78ec9e657e9e4656412d7282aa7ba5c5d26
Tsungai Chipato <tsungai@kryha.io>
2022-06-21 11:51:19 +0200

    add early return

--- frontend/src/components/item-stats/item-stats.tsx
+++ frontend/src/components/item-stats/item-stats.tsx
@@ -15,40 +15,36 @@ interface ItemStatsProps {
 }
 
 export const ItemStats: FC<ItemStatsProps> = ({ item, position, area }) => {
+  if (!item) return <></>;
+
   return (
-    <>
-      {item ? (
-        <StatsWrapper position={position} area={area}>
-          <Header>
-            <Title>{item.name}</Title>
-            <Footer>
-              <Badge>
-                <ButtonText customColor={color.darkGrey}>{item.category}</ButtonText>
-              </Badge>
-              <ButtonText>{text.param.id(item.id)}</ButtonText>
-            </Footer>
-          </Header>
-          <StatsContainer>
-            <HorizontalDivider />
-            <ButtonText customColor={color.darkGrey}>{text.item.level}</ButtonText>
-            <MenuText>{item.level}</MenuText>
-            <ProgressContainer>
-              <ButtonText>{text.item.effectiveness}</ButtonText>
-              <DetailSectionProgress id={text.item.effectiveness} value={item.effectiveness || 0} max={ONE_HUNDRED_PERCENT}>
-                {item.effectiveness}
-              </DetailSectionProgress>
-            </ProgressContainer>
-            <ProgressContainer>
-              <ButtonText>{text.item.layerComplexity}</ButtonText>
-              <DetailSectionProgress id={text.item.layerComplexity} value={item.layerComplexity || 0} max={ONE_HUNDRED_PERCENT}>
-                {text.item.layerComplexity}
-              </DetailSectionProgress>
-            </ProgressContainer>
-          </StatsContainer>
-        </StatsWrapper>
-      ) : (
-        <></>
-      )}
-    </>
+    <StatsWrapper position={position} area={area}>
+      <Header>
+        <Title>{item.name}</Title>
+        <Footer>
+          <Badge>
+            <ButtonText customColor={color.darkGrey}>{item.category}</ButtonText>
+          </Badge>
+          <ButtonText>{text.param.id(item.id)}</ButtonText>
+        </Footer>
+      </Header>
+      <StatsContainer>
+        <HorizontalDivider />
+        <ButtonText customColor={color.darkGrey}>{text.item.level}</ButtonText>
+        <MenuText>{item.level}</MenuText>
+        <ProgressContainer>
+          <ButtonText>{text.item.effectiveness}</ButtonText>
+          <DetailSectionProgress id={text.item.effectiveness} value={item.effectiveness || 0} max={ONE_HUNDRED_PERCENT}>
+            {item.effectiveness}
+          </DetailSectionProgress>
+        </ProgressContainer>
+        <ProgressContainer>
+          <ButtonText>{text.item.layerComplexity}</ButtonText>
+          <DetailSectionProgress id={text.item.layerComplexity} value={item.layerComplexity || 0} max={ONE_HUNDRED_PERCENT}>
+            {text.item.layerComplexity}
+          </DetailSectionProgress>
+        </ProgressContainer>
+      </StatsContainer>
+    </StatsWrapper>
   );
 };
