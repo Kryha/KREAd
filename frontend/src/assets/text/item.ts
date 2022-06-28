060b1d00800761e5a076432afe6ca974d8ba3d1a
Tsungai Chipato <tsungai@kryha.io>
2022-06-28 11:19:50 +0200

    fixed text

--- frontend/src/assets/text/item.ts
+++ frontend/src/assets/text/item.ts
@@ -32,7 +32,7 @@ export const item = {
   zero: 0,
   noItemEquipped: "no item equipped",
   startEquipping: "start equipping",
-  youDidNotEquipp: "you did not equipped any item yet. Your inventory it is empty, start equipping some items and building your own characters!",
+  youDidNotEquipp: "you did not equipped any item yet. Your inventory is empty, start equipping some items and building your own characters!",
   selectAnItemFrom: "select an item from the the store",
   noItemsInInventory: "no items in inventory",
   buyItemsFromStore: "buy items from the store",
