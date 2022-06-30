79ae4098e1ae6655c6215aeea0a525dc4eca6737
Marius Sili <sili.marius.me@gmail.com>
2022-06-30 15:03:39 +0200

    Tracking config again

---
+++ frontend/src/service/conf/defaults.js
@@ -0,0 +1,21 @@
+// GENERATED FROM /Users/marius/Projects/Kryha/Agoric/agoric/api/deploy.js
+export default {
+  INSTANCE_NFT_MAKER_BOARD_ID: "board06120",
+  INSTALLATION_BOARD_ID: "board04719",
+  INVITE_BRAND_BOARD_ID: "board02810",
+  INVITE_ISSUER_BOARD_ID: "board02810",
+  BRIDGE_URL: "agoric-lookup:https://local.agoric.com?append=/bridge",
+  brandBoardIds: {
+    Character: "board02021",
+    Item: "board03523",
+    Money: "board0074",
+  },
+  issuerBoardIds: {
+    Character: "board01422",
+    Item: "board05024",
+    Money: "board0223",
+  },
+  MONEY_DECIMAL_PLACES: 6,
+  API_URL: "http://127.0.0.1:8000",
+  CONTRACT_NAME: "CHARACTER",
+};
---
+++ frontend/src/service/conf/installation-constants-nft-maker.js
@@ -0,0 +1,5 @@
+// GENERATED FROM /Users/marius/Projects/Kryha/Agoric/agoric/contract/deploy.js
+export default {
+  CONTRACT_NAME: "CHARACTER",
+  INSTALLATION_BOARD_ID: "board04719",
+};
