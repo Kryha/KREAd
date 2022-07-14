25f61b4643b86df8870c2f6fee42e9e5c13970e2
Ramiro Ramirez <ramiro@kryha.io>
2022-07-14 13:44:16 +0200

    Add makeHashId utility

--- agoric/contract/src/utils.js
+++ agoric/contract/src/utils.js
@@ -6,3 +6,20 @@ export const sameType = (a, b) => {
   else if (!objectA && !objectB) return typeof a === typeof b;
   else return false;
 };
+
+/* eslint-disable no-bitwise */
+export const makeHashId = (str, seed = 42) => {
+  let h1 = 0xdeadbeef ^ seed;
+  let h2 = 0x41c6ce57 ^ seed;
+  const primeA = 2246822507;
+  const primeB = 3266489909;
+  str.forEach((ch) => {
+    h1 = Math.imul(h1 ^ ch, 2654435761);
+    h2 = Math.imul(h2 ^ ch, 1597334677);
+  });
+  h1 =
+    Math.imul(h1 ^ (h1 >>> 16), primeA) ^ Math.imul(h2 ^ (h2 >>> 13), primeB);
+  h2 =
+    Math.imul(h2 ^ (h2 >>> 16), primeA) ^ Math.imul(h1 ^ (h1 >>> 13), primeB);
+  return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+};
