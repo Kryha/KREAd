4ac62a4a137d59846ed34c3924468bc575593679
Ramiro Ramirez <ramiro@kryha.io>
2022-07-20 13:29:36 +0200

    Fix broken logic

--- frontend/src/service/purses/process.ts
+++ frontend/src/service/purses/process.ts
@@ -4,7 +4,7 @@ import { AgoricDispatch } from "../../interfaces/agoric.interfaces";
 import { CharacterDispatch } from "../../interfaces/character-actions.interfaces";
 import { ItemDispatch } from "../../interfaces/item-actions.interfaces";
 
-// TODO: Add purses interface
+// This fetches assets data from Wallet purses and updates the local context state for characters & items
 export const processPurses = async (
   purses: any[],
   contractPublicFacet: any,
@@ -28,18 +28,14 @@ export const processPurses = async (
   const ownedCharacters = newCharacterPurses.flatMap((purse) => {
     return purse.value;
   });
+  console.log("🚀 ~ file: process.ts ~ line 31 ~ ownedCharacters ~ ownedCharacters", ownedCharacters);
 
   // Map characters to the corresponding inventory in the contract
-  const equippedCharacters = await Promise.all(
+  const charactersWithItems = await Promise.all(
     ownedCharacters.map(async (character: Character) => {
       const {
         items: { value: equippedItems },
       } = await E(contractPublicFacet).getCharacterInventory(character.name);
-
-      const itemsSetEqquiped = equippedItems.map((item: Item) => (item.equippedTo = character.id));
-      console.log("🚀 ~ file: process.ts ~ line 40 ~ ownedCharacters.map ~ itemsSetEqquiped", itemsSetEqquiped);
-
-      itemDispatch({ type: "ADD_OWNED_ITEMS", payload: itemsSetEqquiped });
 
       return {
         ...character,
@@ -58,13 +54,19 @@ export const processPurses = async (
       };
     })
   );
-  // const ownedCharactersWithItems = await Promise.all(characterPromises);
 
-  if (equippedCharacters.length) {
-    characterDispatch({ type: "SET_OWNED_CHARACTERS", payload: equippedCharacters });
-    characterDispatch({ type: "SET_SELECTED_CHARACTER", payload: equippedCharacters[0] });
+  if (charactersWithItems.length) {
+    console.log("🚀 ~ file: process.ts ~ line 59 ~ charactersWithItems", charactersWithItems);
+    characterDispatch({ type: "SET_OWNED_CHARACTERS", payload: charactersWithItems });
+    characterDispatch({ type: "SET_SELECTED_CHARACTER", payload: charactersWithItems[0] });
   }
   characterDispatch({ type: "SET_FETCHED", payload: true });
+
+  // TODO: ADD owned items based on characters inventory
+  // const itemsSetEqquiped = equippedItems.map((item: Item) => (item.equippedTo = character.id));
+  // console.log("🚀 ~ file: process.ts ~ line 40 ~ ownedCharacters.map ~ itemsSetEqquiped", itemsSetEqquiped);
+
+  // itemDispatch({ type: "ADD_OWNED_ITEMS", payload: itemsSetEqquiped });
 
   // Set Items state
   const ownedItems = newItemPurses.flatMap((purse) => {
@@ -72,6 +74,8 @@ export const processPurses = async (
   });
   ownedItems && itemDispatch({ type: "ADD_OWNED_ITEMS", payload: ownedItems });
   itemDispatch({ type: "SET_FETCHED", payload: true });
+
+  // TODO: Add global isLoading and set it to false here
 
   console.info(`👤 Found ${ownedCharacters.length} characters.`);
   console.info(`📦 Found ${ownedItems.length} Items.`);
