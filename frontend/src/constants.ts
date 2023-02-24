c03a81544a7ec723f90ba0c21679290193d65147
Wietze Slagman <wietze@kryha.io>
2023-02-24 08:22:56 +0000

    Merged PR 3728: Change CRA to Vite

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,5 +1,5 @@
 export const GO_BACK = -1;
-export const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5001";
+export const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:5001";
 export const MIN_PRICE = 0;
 export const MAX_PRICE = 10000000;
 export const INFORMATION_STEP = 0;
