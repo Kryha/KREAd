f22b0d5a8516cf86632bb429c95c7f45ac92353d
Privilege Mendes <privilege@kryha.io>
2023-06-21 13:14:52 +0200

    fix: fixed character card info button position and code formatting

--- frontend/src/components/button-info/styles.ts
+++ frontend/src/components/button-info/styles.ts
@@ -2,11 +2,6 @@ import styled from "@emotion/styled";
 import { SecondaryButton } from "../atoms";
 
 export const ButtonInfoWrap = styled.div`
-  display: inline-block;
-  position: absolute;
-  right: 0;
-  margin-right: 20px;
-  margin-bottom: 20px;
   ${SecondaryButton} {
     font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New", monospace;
     text-transform: none;
