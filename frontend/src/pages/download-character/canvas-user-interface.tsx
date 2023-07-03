2c043cfe2abde345f9daec6b11c129275e1c224b
Privilege Mendes <privilege@kryha.io>
2023-07-03 19:20:56 +0200

    refactor: removed duplicate code, now using a context provider as a parent for character building

--- frontend/src/pages/download-character/canvas-user-interface.tsx
+++ frontend/src/pages/download-character/canvas-user-interface.tsx
@@ -1,8 +1,12 @@
 import React, { FC, useState } from "react";
 import {
+  ControlAreaBody,
   ControlAreaBottom,
+  ControlAreaBottomWrapper,
   ControlAreaTop,
   ControlAreaWrapper,
+  Download,
+  DownloadButton,
   ZoomActions,
   ZoomContainer,
   ZoomIn,
@@ -15,17 +19,13 @@ import { PrimaryButton } from "../../components";
 import { routes } from "../../navigation";
 import { useNavigate } from "react-router-dom";
 import { DownloadImageModal } from "../../components/download-image/download-image-modal";
+import { useZoomCanvas } from "../../hooks/use-zoom-canvas";
 
-interface Props {
-  scale: number;
-  handleZoomIn: () => void;
-  handleZoomOut: () => void;
-  handleResetZoom: () => void;
-}
-
-export const CanvasUserInterface: FC<Props> = ({ scale, handleZoomIn, handleZoomOut, handleResetZoom }) => {
+export const CanvasUserInterface: FC = () => {
   const [isDownloadOpen, setIsDownloadOpen] = useState(false);
   const navigate = useNavigate();
+  const { scale, handleZoomIn, handleZoomOut, handleResetZoom } = useZoomCanvas();
+  const zoomPercentage = Math.round(scale * 100);
 
   const handleDownloadButtonClick = () => {
     setIsDownloadOpen(true);
@@ -41,24 +41,30 @@ export const CanvasUserInterface: FC<Props> = ({ scale, handleZoomIn, handleZoom
 
   return (
     <ControlAreaWrapper>
+      <ControlAreaBody>
+        <DownloadImageModal isOpen={isDownloadOpen} onClose={handleCloseDownload} />
+      </ControlAreaBody>
       <ControlAreaTop>
         <PrimaryButton onClick={handleHome}>Home</PrimaryButton>
       </ControlAreaTop>
-      <ControlAreaBottom>
-        <DownloadImageModal isOpen={isDownloadOpen} onClose={handleCloseDownload} />
-        <PrimaryButton onClick={handleDownloadButtonClick}>Download</PrimaryButton>
-        <ZoomContainer>
-          <ZoomActions>
-            <ZoomOutButton onClick={handleZoomOut}>
-              <ZoomOut />
-            </ZoomOutButton>
-            <ZoomResetButton onClick={handleResetZoom}>{Math.round(scale * 100)}%</ZoomResetButton>
-            <ZoomInButton onClick={handleZoomIn}>
-              <ZoomIn />
-            </ZoomInButton>
-          </ZoomActions>
-        </ZoomContainer>
-      </ControlAreaBottom>
+      <ControlAreaBottomWrapper>
+        <ControlAreaBottom>
+          <DownloadButton onClick={handleDownloadButtonClick}>
+            <Download />
+          </DownloadButton>
+          <ZoomContainer>
+            <ZoomActions>
+              <ZoomOutButton onClick={handleZoomOut}>
+                <ZoomOut />
+              </ZoomOutButton>
+              <ZoomResetButton onClick={handleResetZoom}>{zoomPercentage}%</ZoomResetButton>
+              <ZoomInButton onClick={handleZoomIn}>
+                <ZoomIn />
+              </ZoomInButton>
+            </ZoomActions>
+          </ZoomContainer>
+        </ControlAreaBottom>
+      </ControlAreaBottomWrapper>
     </ControlAreaWrapper>
   );
 };
--- frontend/src/pages/download-character/download-character.tsx
+++ frontend/src/pages/download-character/download-character.tsx
@@ -1,119 +1,70 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import React, { useEffect } from "react";
 import { useViewport } from "../../hooks";
-import { Empty, TempetCharacter, text } from "../../assets";
 import { Layer, Stage } from "react-konva";
-import { useSelectedCharacter } from "../../service";
-import { LoadingPage } from "../../components";
-import Konva from "konva";
-import { useZoomCanvas } from "../../hooks/use-zoom-canvas";
 import { CanvasArea } from "./styles";
+import { useCharacterImage } from "../../context/character-image-provider";
+import Konva from "konva";
 import { CanvasUserInterface } from "./canvas-user-interface";
+import { text } from "../../assets";
 
 export const DownloadCharacter = () => {
   const { width, height } = useViewport();
-  const [selectedCharacter, isLoading] = useSelectedCharacter();
+  const { stageRef, layerRef, drawCharacter } = useCharacterImage();
 
-  const character = useMemo(() => selectedCharacter?.nft, [selectedCharacter]);
-  const items = useMemo(() => selectedCharacter?.equippedItems || {}, [selectedCharacter]);
-
-  const stageRef = useRef<Konva.Stage>(null);
-  const layerRef = useRef<Konva.Layer>(null);
-
-  const [resetZoom, setResetZoom] = useState(false);
-  const { scale, handleZoomIn, handleZoomOut, handleResetZoom } = useZoomCanvas(stageRef, resetZoom, setResetZoom);
-
-  Konva.hitOnDragEnabled = true;
-
-  /* Set the maximum canvas size to 800 x 1000 */
-  const stageWidth = Math.min(width, 800);
-  const stageHeight = Math.min(height, 1000);
-  const scaledWidth = stageWidth * scale;
-  const scaledHeight = stageHeight * scale;
+  const positionX = width / 2;
+  const positionY = height / 2;
 
   useEffect(() => {
-    const drawCharacter = async () => {
-      layerRef.current?.destroyChildren();
+    layerRef.current?.destroyChildren();
 
-      const loadImage = (src: string, alt: string, drawWidth: number, drawHeight: number): Promise<Konva.Image> => {
-        return new Promise((resolve, reject) => {
-          const imageObj = new window.Image();
-          imageObj.src = src;
-          imageObj.crossOrigin = "anonymous"; // This enables CORS
-          imageObj.onload = () => {
-            const x = (width - drawWidth) / 2;
-            const y = (height - drawHeight) / 2;
-            const konvaImage = new Konva.Image({
-              image: imageObj,
-              x: x,
-              y: y,
-              width: drawWidth,
-              height: drawHeight,
-              preventDefault: false,
-            });
+    const drawImages = async () => {
+      try {
+        if (layerRef.current) {
+          const character = await drawCharacter();
+          const scaledCharacter = scaleCharacterToScreen(character, width, height, positionX, positionY);
 
-            resolve(konvaImage);
-          };
-
-          imageObj.onerror = () => {
-            reject(new Error(`Failed to load image: ${src}`));
-          };
-        });
-      };
-
-      const drawOrder = [
-        { src: items.background?.image || Empty, alt: items.background?.name || text.character.background },
-        { src: items.midBackground?.image || Empty, alt: items.midBackground?.name || text.character.midBackground },
-        { src: character?.image || TempetCharacter, alt: character?.name || text.character.defaultCharacter },
-        { src: items.hair?.image || Empty, alt: items.hair?.name || text.character.hair },
-        { src: items.mask?.image || Empty, alt: items.mask?.name || text.character.mask },
-        { src: items.noseline?.image || Empty, alt: items.noseline?.name || text.character.noseline },
-        { src: items.liquid?.image || Empty, alt: items.liquid?.name || text.character.liquid },
-        { src: items.frontMask?.image || Empty, alt: items.frontMask?.name || text.character.frontMask },
-        { src: items.airReservoir?.image || Empty, alt: items.airReservoir?.name || text.character.airReservoir },
-        { src: items.clothing?.image || Empty, alt: items.clothing?.name || text.character.clothing },
-        { src: items.headPiece?.image || Empty, alt: items.headPiece?.name || text.character.headPiece },
-      ];
-
-      const drawnImages: Konva.Image[] = [];
-
-      for (const { src, alt } of drawOrder) {
-        const completedCharacter = await loadImage(src, alt, scaledWidth, scaledHeight);
-        drawnImages.push(completedCharacter);
-      }
-
-      if (drawnImages.length > 0) {
-        const imageGroup = new Konva.Group({
-          preventDefault: false,
-        });
-        drawnImages.forEach((image) => imageGroup.add(image));
-
-        imageGroup.setAttrs({
-          width: scaledWidth,
-          height: scaledHeight,
-        });
-
-        layerRef.current?.add(imageGroup);
-        layerRef.current?.batchDraw();
+          layerRef.current.add(scaledCharacter);
+          layerRef.current.batchDraw();
+        }
+      } catch (error) {
+        console.error(text.error.sorrySomethingWentWrong, error);
       }
     };
 
-    drawCharacter();
-  }, [character, items, width, height, scale]);
+    drawImages();
+  }, [drawCharacter, width, height, positionX, positionY, layerRef]);
 
-  if (isLoading) {
-    return <LoadingPage spinner={false} />;
+  return (
+    <CanvasArea>
+      <CanvasUserInterface />
+      <Stage ref={stageRef} width={width} height={height} draggable={true}>
+        <Layer ref={layerRef} />
+      </Stage>
+    </CanvasArea>
+  );
+};
+
+const scaleCharacterToScreen = (group: Konva.Group, width: number, height: number, x: number, y: number) => {
+  const aspectRatio = group.height() / group.width();
+  const stageAspectRatio = height / width;
+  let newWidth = width;
+  let newHeight = height;
+
+  if (stageAspectRatio > aspectRatio) {
+    newHeight = width * aspectRatio;
+  } else {
+    newWidth = height / aspectRatio;
   }
 
-  if (selectedCharacter) {
-    return (
-      <CanvasArea>
-        <CanvasUserInterface scale={scale} handleZoomIn={handleZoomIn} handleZoomOut={handleZoomOut} handleResetZoom={handleResetZoom} />
-        <Stage width={width} height={height} ref={stageRef} draggable={true}>
-          <Layer ref={layerRef} />
-        </Stage>
-      </CanvasArea>
-    );
-  }
+  const scaleX = newWidth / group.width();
+  const scaleY = newHeight / group.height();
 
-  return <LoadingPage spinner={true} />;
+  group.setAttrs({
+    scaleX: scaleX,
+    scaleY: scaleY,
+    x,
+    y,
+  });
+
+  return group;
 };
