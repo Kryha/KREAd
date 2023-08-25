4cc50a128e57344327c182dd6a00c6e98a838d41
Privilege Mendes <privilege@kryha.io>
2023-08-25 13:30:40 +0200

    feat: created generic assets filtering, cards and details features to be used at shop and inventory

---
+++ frontend/src/components/asset-card/asset-card.tsx
@@ -0,0 +1,74 @@
+import { isCharacterCategory, isItemCategory } from "../../interfaces";
+import { BoldLabel, Dash, ImageProps } from "../atoms";
+import { FC } from "react";
+import {
+  AssetContent,
+  AssetFooter,
+  AssetImage,
+  AssetImageContainer,
+  AssetInfoContainer,
+  AssetTag,
+  AssetTagPrice,
+  AssetTitleText,
+  AssetTitleWrapper,
+  AssetWrapper,
+  NoAssetImage,
+} from "./styles";
+import { text } from "../../assets";
+import { color } from "../../design";
+import { PriceInIst } from "../price-in-ist";
+import { AssetData } from "../asset-cards/asset-cards";
+import { SECTION } from "../../constants";
+
+interface Props {
+  data: AssetData;
+  onClick?: (assetId: string) => void;
+  imageProps?: ImageProps;
+  section: typeof SECTION[keyof typeof SECTION];
+}
+export const AssetCard: FC<Props> = ({ data, onClick, section }) => {
+  const handleClick = () => {
+    onClick && onClick(data.id);
+  };
+
+  if (!isItemCategory(data.category) && !isCharacterCategory(data.category)) return <></>;
+
+  return (
+    <AssetWrapper onClick={() => handleClick()}>
+      <AssetContent>
+        <AssetImageContainer>
+          {data.image ? <AssetImage src={data.image} category={data.category} /> : <NoAssetImage />}
+        </AssetImageContainer>
+        <AssetInfoContainer>
+          <AssetTitleWrapper>
+            <AssetTitleText>{data.name}</AssetTitleText>
+            <BoldLabel>{text.param.categories[data.category]}</BoldLabel>
+          </AssetTitleWrapper>
+          <AssetFooter>
+            {section === SECTION.INVENTORY && (
+              <>
+                <AssetTag>
+                  <BoldLabel customColor={color.black}>{text.param.level(data.level)}</BoldLabel>
+                  <Dash />
+                  <BoldLabel customColor={color.black}>{text.param.rarity(data.rarity)}</BoldLabel>
+                </AssetTag>
+                {data.isEquipped && <BoldLabel customColor={color.black}>{text.general.equipped}</BoldLabel>}
+                {data.isForSale && <BoldLabel customColor={color.black}>{text.general.forSale}</BoldLabel>}
+              </>
+            )}
+            {section === SECTION.SHOP && (
+              <>
+                <AssetTag>
+                  <BoldLabel customColor={color.black}>{text.param.level(data.level)}</BoldLabel>
+                  <Dash />
+                  <BoldLabel customColor={color.black}>{text.param.rarity(data.rarity)}</BoldLabel>
+                </AssetTag>
+                <AssetTagPrice>{<PriceInIst price={Number(data.price)} />}</AssetTagPrice>
+              </>
+            )}
+          </AssetFooter>
+        </AssetInfoContainer>
+      </AssetContent>
+    </AssetWrapper>
+  );
+};
