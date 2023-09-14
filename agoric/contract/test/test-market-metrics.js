39cea0d3ce0d1e09c61dfe209650bf7098e0ff2e
Pandelis Symeonidis <pandelis@kryha.io>
2023-09-14 12:13:11 +0000

    Merged PR 4423: Royalties
    - Implemented royalty fee for items + characters.
    - Get depositFacet of a wallet (currently hardcoded in the proposal) and pass it to the contract as privateArg
    - Pass `royaltyRate` as private arg
    - Updated type guard and tests
    - Added check that the brand of the payment is IST (created new token (fakeIST) for tests)
    
    When a NFT is put for sale, the royalty fee is calculated and added to the market entry.
    For someone to buy this NFT, he has to give at least `askingPrice + royalty`. First we transfer the `royalty` to the `depositFacet` and then the remaining amount is given to the seller.
    
    Related work items: #19318

--- agoric/contract/test/test-market-metrics.js
+++ agoric/contract/test/test-market-metrics.js
@@ -12,6 +12,7 @@ async function sellCharacter(context, user, characterName, askingPrice) {
     instance: { publicFacet },
     contractAssets,
     zoe,
+    paymentAsset,
   } = context;
 
   const characterToSell = user
@@ -22,10 +23,7 @@ async function sellCharacter(context, user, characterName, askingPrice) {
     contractAssets.character.brand,
     copyBagAmount,
   );
-  const priceAmount = AmountMath.make(
-    contractAssets.payment.brand,
-    askingPrice,
-  );
+  const priceAmount = AmountMath.make(paymentAsset.brandMockIST, askingPrice);
 
   const sellCharacterInvitation = await E(
     publicFacet,
@@ -65,7 +63,10 @@ async function buyCharacter(context, user, characterName, seller) {
     contractAssets.character.brand,
     copyBagAmount,
   );
-  const priceAmount = characterToBuy.askingPrice;
+  const priceAmount = AmountMath.add(
+    AmountMath.add(characterToBuy.askingPrice, characterToBuy.royalty),
+    characterToBuy.platformFee,
+  );
 
   const buyCharacterInvitation = await E(
     publicFacet,
@@ -92,22 +93,14 @@ async function buyCharacter(context, user, characterName, seller) {
 
 test.before(async (t) => {
   const bootstrap = await bootstrapContext();
-
-  const { zoe, contractAssets, assets, purses, instance } = bootstrap;
+  const { zoe, contractAssets, assets, purses, instance, paymentAsset } =
+    bootstrap;
 
   const bob = makeKreadUser('bob', purses);
 
-  const topUpInvitation = await E(
-    instance.publicFacet,
-  ).makeTokenFacetInvitation();
-
-  const proposal = harden({
-    want: {
-      Asset: AmountMath.make(contractAssets.payment.brand, harden(100n)),
-    },
-  });
-  const userSeat = await E(zoe).offer(topUpInvitation, proposal);
-  const payout = await E(userSeat).getPayout('Asset');
+  const payout = paymentAsset.mintMockIST.mintPayment(
+    AmountMath.make(paymentAsset.brandMockIST, harden(100n)),
+  );
   bob.depositPayment(payout);
 
   t.context = {
@@ -117,6 +110,7 @@ test.before(async (t) => {
     purses,
     zoe,
     users: { bob },
+    paymentAsset,
   };
 });
 
@@ -213,11 +207,9 @@ test.serial('---| METRICS - Amount sold character', async (t) => {
       },
     },
   } = flow;
-
   await buyCharacter(t.context, bob, characterName, bob);
 
   const metrics = await E(publicFacet).getMarketMetrics();
-  await E(publicFacet).getMarketMetrics();
 
   const character = bob.getCharacters().find((c) => c.name === characterName);
 
