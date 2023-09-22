993b0d72abccd8fe5a17a0360e203e2b1d9dc982
Dan Connolly <dckc@madmode.com>
2023-09-22 15:38:24 -0500

    feat: parameterize royaltyAddr, platformFeeAddr

--- agoric/contract/src/proposal/start-kread-script.js
+++ agoric/contract/src/proposal/start-kread-script.js
@@ -1,15 +1,32 @@
+/* global process */
 import { makeHelpers } from '@agoric/deploy-script-support';
 
 import { getManifestForStartKread } from './start-kread-proposal.js';
 
+const fail = (msg) => {
+  throw Error(msg);
+};
+
 // Build proposal for sim-chain etc.
 /** @type {import('@agoric/deploy-script-support/src/externalTypes.js').ProposalBuilder} */
-export const defaultProposalBuilder = async ({ publishRef, install }) => {
+export const defaultProposalBuilder = async (
+  { publishRef, install },
+  options = {},
+) => {
+  const theEnv = (name) => process.env[name] || fail(`$${name} required`);
+
+  const {
+    royaltyAddr = theEnv('KREAD_ROYALTY_ADDRESS'),
+    platformFeeAddr = theEnv('KREAD_PLATFORM_ADDRESS'),
+  } = options;
+
   return harden({
     sourceSpec: './start-kread-proposal.js',
     getManifestCall: [
       getManifestForStartKread.name,
       {
+        royaltyAddr,
+        platformFeeAddr,
         kreadKitRef: publishRef(
           install('../index.js', '../bundles/bundle-kreadKit.js', {
             persist: true,
