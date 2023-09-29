ea13a30e0b6e345265cec52958f51a1746e9ce13
Pandelis Symeonidis <pandelis@kryha.io>
2023-09-29 12:37:28 +0200

    fix

--- frontend/src/pages/shop/characters-shop.tsx
+++ frontend/src/pages/shop/characters-shop.tsx
@@ -26,20 +26,20 @@ export const CharactersShop: FC<Props> = ({ pageSelector }) => {
   const assetsCount = characters.length;
 
   const metricsLabels = ["Sales", "Collection size", "Floor price", "Avg. character price", "Avg. character level", "Avg. marketplace level"];
-  const metricsData = [
+  const metricsData = metrics ? [
     metrics.amountSold,
     metrics.collectionSize,
     "IST " + Math.min(...characters.map((x) => uISTToIST(Number(x.sell.price)))),
     "IST " + (characters.reduce((acc, x) => acc + uISTToIST(Number(x.sell.price)), 0) / characters.length).toFixed(2),
     metrics.averageLevel.toFixed(2),
-    metrics.marketplaceAverageLevel.toFixed(2)]
+    metrics.marketplaceAverageLevel.toFixed(2)] : []
   
 
   return (
     <>
       <AssetCharacterFilters section={SECTION.SHOP} pageSelector={pageSelector} />
       <AssetFilterCount customColor={color.darkGrey}>Inventory: {text.param.amountOfCharacters(assetsCount)}</AssetFilterCount>
-      {metricsData ? <MarketplaceMetrics header={metricsLabels} data={metricsData} /> : <></>}
+      {metrics ? <MarketplaceMetrics header={metricsLabels} data={metricsData} /> : <></>}
       <HorizontalDivider />
       {selectedId && <CharacterDetailsMarket characterInMarket={character} selectCharacter={(id: string) => setSelectedId(id)} />}
       {characters.length > 0 ? (
--- frontend/src/pages/shop/items-shop.tsx
+++ frontend/src/pages/shop/items-shop.tsx
@@ -25,20 +25,20 @@ export const ItemsShop: FC<Props> = ({ pageSelector }) => {
   const assetsCount = items.length;
 
   const metricsLabels = ["Sales", "Collection size", "Floor price", "Avg. item price", "Avg. item level", "Avg. marketplace level"];
-  const metricsData = [
+  const metricsData = metrics ? [
     metrics.amountSold,
     metrics.collectionSize,
     "IST " + Math.min(...items.map((x) => uISTToIST(Number(x.sell.price)))),
     "IST " + (items.reduce((acc, x) => acc + uISTToIST(Number(x.sell.price)), 0) / items.length).toFixed(2),
     metrics.averageLevel.toFixed(2),
-    metrics.marketplaceAverageLevel.toFixed(2)]
+    metrics.marketplaceAverageLevel.toFixed(2)] : []
   
   if (!items) return <></>;
   return (
     <>
       <AssetItemFilters section={SECTION.SHOP} pageSelector={pageSelector} />
       <AssetFilterCount customColor={color.darkGrey}>Inventory: {text.param.amountOfItems(assetsCount)}</AssetFilterCount>
-      {items.length > 0 ? <MarketplaceMetrics header={metricsLabels} data={metricsData} /> : <></>}
+      {metrics ? <MarketplaceMetrics header={metricsLabels} data={metricsData} /> : <></>}
       <HorizontalDivider />
       {selectedId && item && <ItemDetailsMarket itemInMarket={item} selectItemInMarket={(id: string) => setSelectedId(id)} />}
       {items.length > 0 ? (
