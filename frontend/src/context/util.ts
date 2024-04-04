92a3c78730e3376640848b673844a418c8f2499a
JorgeLopes-BytePitch <jorge.lopes@bytepitch.com>
2024-04-04 12:48:10 +0100

    fix(legacy queries): update the arguments being passed to makeAgoricChainStorageWatcher

--- frontend/src/context/util.ts
+++ frontend/src/context/util.ts
@@ -1,10 +1,12 @@
 export const fetchChainInfo = async (netconfigURL: string) => {
   const response = await fetch(netconfigURL, {
-    headers: { accept: "application/json" },
+    headers: { accept: 'application/json' },
   });
-  const { rpcAddrs, chainName } = await response.json();
+  const { rpcAddrs, chainName, apiAddrs } = await response.json();
+
   return {
     rpc: rpcAddrs[Math.floor(Math.random() * rpcAddrs.length)],
+    api: apiAddrs[Math.floor(Math.random() * apiAddrs.length)],
     chainName,
   };
 };
